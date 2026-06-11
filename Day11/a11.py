3794a5014ffe7fe240649202a895856984e2d213
suriyansh0409 <surisingh0218@gmail.com>
2026-06-11 11:15:27 +0530

    Create a11.py

---
+++ Day11/a11.py
@@ -0,0 +1,7 @@
+#Program to Write function to find sum of two numbers.
+def sumof(x,y):
+    return(x+y)
+
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+print(sumof(x,y))
